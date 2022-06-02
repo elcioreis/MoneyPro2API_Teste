3c3782bb176c113822784bd02452c65ae753de37
Elcio Reis <elcio.reis@outlook.com>
2022-06-02 19:10:14 -0300

    Refaturamento finalizado

--- Controllers/InstitutionTypeController.cs
+++ Controllers/InstitutionTypeController.cs
@@ -19,25 +19,22 @@ public class InstitutionTypeController : ControllerBase
     public async Task<IActionResult> GetAllAsync(
         [FromServices] MoneyDataContext context)
     {
-        if (Int32.TryParse(User.FindFirst(ClaimTypes.NameIdentifier).Value, out int userId))
-        {
-            try
-            {
-                var institutionTypes = await context.InstitutionTypes
-                    .AsNoTracking()
-                    .Where(x => x.UserId == userId)
-                    .OrderBy(x => x.Nickname).ToListAsync();
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<List<InstitutionType>>("E04X00 - Não autorizado"));
 
-                return Ok(new ResultViewModel<List<InstitutionType>>(institutionTypes));
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, new ResultViewModel<List<InstitutionType>>("E04X00 - Falha interna no servidor"));
-            }
-        }
-        else
+        try
         {
-            return StatusCode(401, new ResultViewModel<List<InstitutionType>>("E04X01 - Não autorizado"));
+            var institutionTypes = await context.InstitutionTypes
+                .AsNoTracking()
+                .Where(x => x.UserId == userId)
+                .OrderBy(x => x.Nickname)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<List<InstitutionType>>(institutionTypes));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<InstitutionType>>("E04X01 - Falha interna no servidor"));
         }
     }
 
@@ -46,25 +43,48 @@ public class InstitutionTypeController : ControllerBase
     public async Task<IActionResult> GetActiveAsync(
        [FromServices] MoneyDataContext context)
     {
-        if (Int32.TryParse(User.FindFirst(ClaimTypes.NameIdentifier).Value, out int userId))
-        {
-            try
-            {
-                var institutionTypes = await context.InstitutionTypes
-                    .AsNoTracking()
-                    .Where(x => x.UserId == userId && x.Active)
-                    .OrderBy(x => x.Nickname).ToListAsync();
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<List<InstitutionType>>("E04X02 - Não autorizado"));
 
-                return Ok(new ResultViewModel<List<InstitutionType>>(institutionTypes));
-            }
-            catch (Exception)
-            {
-                return StatusCode(500, new ResultViewModel<List<InstitutionType>>("E04X00 - Falha interna no servidor"));
-            }
-        }
-        else
+        try
         {
-            return StatusCode(401, new ResultViewModel<List<InstitutionType>>("E04X01 - Não autorizado"));
+            var institutionTypes = await context.InstitutionTypes
+                .AsNoTracking()
+                .Where(x => x.UserId == userId && x.Active)
+                .OrderBy(x => x.Nickname)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<List<InstitutionType>>(institutionTypes));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<List<InstitutionType>>("E04X03 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/institutiontype/{id:int}")]
+    [Authorize(Roles = "user")]
+    public async Task<IActionResult> GetByIdAsync(
+        [FromRoute] int id,
+        [FromServices] MoneyDataContext context)
+    {
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<InstitutionType>("E04X04 - Não autorizado"));
+
+        try
+        {
+            var institutiontype = await context.InstitutionTypes
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
+
+            if (institutiontype == null)
+                return NotFound(new ResultViewModel<InstitutionType>("E04X05 - Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<InstitutionType>(institutiontype));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<InstitutionType>("E04X06 - Falha interna do servidor"));
         }
     }
 
@@ -74,11 +94,11 @@ public class InstitutionTypeController : ControllerBase
         [FromBody] InstitutionTypeViewModel model,
         [FromServices] MoneyDataContext context)
     {
-        if (!ModelState.IsValid)
-            return BadRequest(new ResultViewModel<InstitutionType>(ModelState.GetErrors("E04X02 - Conteúdo mal formatado")));
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<InstitutionType>("E04X07 - Não autorizado"));
 
-        if (!Int32.TryParse(User.FindFirst(ClaimTypes.NameIdentifier).Value, out int userId))
-            return StatusCode(401, new ResultViewModel<List<InstitutionType>>("E04X03 - Não autorizado"));
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<InstitutionType>(ModelState.GetErrors("E04X08 - Conteúdo mal formatado")));
 
         var institutionType = new InstitutionType()
         {
@@ -90,52 +110,7 @@ public class InstitutionTypeController : ControllerBase
 
         try
         {
-            _ = await context.InstitutionTypes.AddAsync(institutionType);
-            await context.SaveChangesAsync();
-
-            return Ok(new ResultViewModel<InstitutionType>(institutionType));
-        }
-        catch (DbUpdateException ex)
-        {
-            // Usa Regex para pegar o conteúdo duplicado do banco de dados, que vem entre parênteses do Sql Server
-            Regex filtro = new Regex(@"(.*\()|(\).*)", RegexOptions.Singleline);
-            var chave = filtro.Replace(ex.InnerException?.Message ?? "", "");
-
-            if (!string.IsNullOrEmpty(chave))
-                return StatusCode(400, new ResultViewModel<InstitutionType>($"E04X04 - Tipo de instituição já cadastrada para esse usuário: {chave}"));
-            else
-                return StatusCode(400, new ResultViewModel<InstitutionType>("E04X04 - Tipo de instituição já cadastrada para esse usuário"));
-        }
-        catch (Exception)
-        {
-            return StatusCode(500, new ResultViewModel<InstitutionType>("E04X05 - Falha interna no servidor"));
-        }
-    }
-
-    [HttpPut("v1/institutiontype/{id:int}")]
-    [Authorize(Roles = "user")]
-    public async Task<IActionResult> Update(
-        [FromRoute] int id,
-        [FromBody] InstitutionTypeViewModel model,
-        [FromServices] MoneyDataContext context)
-    {
-        if (!ModelState.IsValid)
-            return BadRequest(new ResultViewModel<InstitutionType>(ModelState.GetErrors("E04X06 - Conteúdo mal formatado")));
-
-        if (!Int32.TryParse(User.FindFirst(ClaimTypes.NameIdentifier).Value, out int userId))
-            return StatusCode(401, new ResultViewModel<List<InstitutionType>>("E04X07 - Não autorizado"));
-
-        try
-        {
-            var institutionType = await context.InstitutionTypes.FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
-
-            if (institutionType == null)
-                return NotFound(new ResultViewModel<InstitutionType>("E04X08 - Conteúdo não encontrado"));
-
-            institutionType.Nickname = model.Nickname;
-            institutionType.Description = model.Description;
-
-            context.InstitutionTypes.Update(institutionType);
+            await context.InstitutionTypes.AddAsync(institutionType);
             await context.SaveChangesAsync();
 
             return Ok(new ResultViewModel<InstitutionType>(institutionType));
@@ -157,21 +132,68 @@ public class InstitutionTypeController : ControllerBase
         }
     }
 
+    [HttpPut("v1/institutiontype/{id:int}")]
+    [Authorize(Roles = "user")]
+    public async Task<IActionResult> Update(
+        [FromRoute] int id,
+        [FromBody] InstitutionTypeViewModel model,
+        [FromServices] MoneyDataContext context)
+    {
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<InstitutionType>("E04X11 - Não autorizado"));
+
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<InstitutionType>(ModelState.GetErrors("E04X12 - Conteúdo mal formatado")));
+
+        try
+        {
+            var institutionType = await context.InstitutionTypes
+                .FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
+
+            if (institutionType == null)
+                return NotFound(new ResultViewModel<InstitutionType>("E04X13 - Conteúdo não encontrado"));
+
+            institutionType.Nickname = model.Nickname;
+            institutionType.Description = model.Description;
+
+            context.InstitutionTypes.Update(institutionType);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<InstitutionType>(institutionType));
+        }
+        catch (DbUpdateException ex)
+        {
+            // Usa Regex para pegar o conteúdo duplicado do banco de dados, que vem entre parênteses do Sql Server
+            Regex filtro = new Regex(@"(.*\()|(\).*)", RegexOptions.Singleline);
+            var chave = filtro.Replace(ex.InnerException?.Message ?? "", "");
+
+            if (!string.IsNullOrEmpty(chave))
+                return StatusCode(400, new ResultViewModel<InstitutionType>($"E04X14 - Tipo de instituição já cadastrada para esse usuário: {chave}"));
+            else
+                return StatusCode(400, new ResultViewModel<InstitutionType>("E04X14 - Tipo de instituição já cadastrada para esse usuário"));
+        }
+        catch (Exception)
+        {
+            return StatusCode(500, new ResultViewModel<InstitutionType>("E04X15 - Falha interna no servidor"));
+        }
+    }
+
     [HttpDelete("v1/institutiontype/{id:int}")]
     [Authorize(Roles = "user")]
     public async Task<IActionResult> Delete(
         [FromRoute] int id,
         [FromServices] MoneyDataContext context)
     {
-        if (!Int32.TryParse(User.FindFirst(ClaimTypes.NameIdentifier).Value, out int userId))
-            return StatusCode(401, new ResultViewModel<List<InstitutionType>>("E04X11 - Não autorizado"));
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<string>("E04X16 - Não autorizado"));
 
         try
         {
-            var institutionType = await context.InstitutionTypes.FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
+            var institutionType = await context.InstitutionTypes
+                .FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
 
             if (institutionType == null)
-                return NotFound(new ResultViewModel<InstitutionType>("E04X12 - Conteúdo não encontrado"));
+                return NotFound(new ResultViewModel<string>("E04X17 - Conteúdo não encontrado"));
 
             context.InstitutionTypes.Remove(institutionType);
             await context.SaveChangesAsync();
@@ -183,31 +205,32 @@ public class InstitutionTypeController : ControllerBase
             var msg = ex.InnerException?.Message;
 
             if (!string.IsNullOrEmpty(msg))
-                return StatusCode(400, new ResultViewModel<Coin>($"E04X13 - Impossível excluir o tipo de instituição: {msg}"));
+                return StatusCode(400, new ResultViewModel<string>($"E04X18 - Impossível excluir o tipo de instituição: {msg}"));
             else
-                return StatusCode(400, new ResultViewModel<Coin>("E04X13 - Impossível excluir o tipo de instituição"));
+                return StatusCode(400, new ResultViewModel<string>("E04X18 - Impossível excluir o tipo de instituição"));
         }
         catch (Exception)
         {
-            return StatusCode(500, new ResultViewModel<Coin>("E04X14 - Falha interna no servidor"));
+            return StatusCode(500, new ResultViewModel<string>("E04X19 - Falha interna no servidor"));
         }
     }
 
     [HttpPut("v1/institutiontype/active/{id:int}")]
     [Authorize(Roles = "user")]
-    public async Task<IActionResult> ChangeActiveAsync(
+    public async Task<IActionResult> ChangeActiveByIdAsync(
         [FromRoute] int id,
         [FromServices] MoneyDataContext context)
     {
-        if (!Int32.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
-            return StatusCode(401, new ResultViewModel<InstitutionType>("E04X15 - Não autorizado"));
+        if (!int.TryParse(User.FindFirstValue(ClaimTypes.NameIdentifier), out int userId))
+            return Unauthorized(new ResultViewModel<InstitutionType>("E04X20 - Não autorizado"));
 
         try
         {
-            var institutionType = await context.InstitutionTypes.FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
+            var institutionType = await context.InstitutionTypes
+                .FirstOrDefaultAsync(x => x.Id == id && x.UserId == userId);
 
             if (institutionType == null)
-                return NotFound(new ResultViewModel<InstitutionType>("E04X16 - Conteúdo não encontrado"));
+                return NotFound(new ResultViewModel<InstitutionType>("E04X21 - Conteúdo não encontrado"));
 
             institutionType.Active = !institutionType.Active;
 
@@ -218,7 +241,7 @@ public class InstitutionTypeController : ControllerBase
         }
         catch (Exception)
         {
-            return StatusCode(500, new ResultViewModel<InstitutionType>("E04X17 - Falha interna no servidor"));
+            return StatusCode(500, new ResultViewModel<InstitutionType>("E04X22 - Falha interna no servidor"));
         }
     }
 }
